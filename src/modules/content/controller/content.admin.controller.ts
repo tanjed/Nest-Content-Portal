0a01a1f5f92f187b17250234e4e6cae35bfb26c0
tanjed.khan <tanjed.atono@gmail.com>
2026-02-04 20:58:10 +0600

    feat: enhance ContentAdminController with getPostById and getAllPosts methods; update service interface to include find and findAll

--- src/modules/content/controller/content.admin.controller.ts
+++ src/modules/content/controller/content.admin.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, Inject, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Inject, NotFoundException, Post, UsePipes } from '@nestjs/common';
 import { CreateContentDto } from '../dto/create-content.dto';
 import { SlugGeneratorPipe } from '../pipe/slug.generator.pipe';
 import type { ContentServiceInterface } from '../service/content.service.interface';
 import { CONTENT_SERVICE_INTERFACE } from '../service/content.service.interface';
 
 
-@Controller('content')
+@Controller('admin/content')
 export class ContentAdminController {
   constructor(
     @Inject(CONTENT_SERVICE_INTERFACE)
     private readonly contentService: ContentServiceInterface
   ){}
 
+  @Get(':id')
+  async getPostById(id: string) {
+    const content = await this.contentService.find(id);
+
+    if (!content) {
+      throw new NotFoundException(`Content with ID ${id} not found`);
+    }
+
+    return content;
+  }
+
+  @Get()
+  async getAllPosts() {
+    return this.contentService.findAll();
+  }
+
   @Post()
   @UsePipes(SlugGeneratorPipe)
   async create(@Body() createContentDto: CreateContentDto) {
     return this.contentService.create(createContentDto);
   }
+
+  async update() {}
+  async delete() {}
 }
--- src/modules/content/service/content.service.interface.ts
+++ src/modules/content/service/content.service.interface.ts
@@ -1,8 +1,11 @@
+import { NotFoundException } from "@nestjs/common"
 import { CreateContentDto } from "../dto/create-content.dto"
 import { Content } from "../entities/content.entity"
 
 export const CONTENT_SERVICE_INTERFACE = Symbol('CONTENT_SERVICE_INTERFACE')
 
 export interface ContentServiceInterface {
+    find(id: string): Promise<Content | null>
+    findAll(): Promise<Content[]>
     create(createContentDto: CreateContentDto): Promise<Content>
 }
